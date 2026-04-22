b586f3d7a3d242c93bd07cc18af2418cd3715c69
nahomnm <tnahom65@gmail.com>
2026-04-23 01:07:11 +0200

    Implemented dynamic header

---
+++ src/utils/renderHeader.js
@@ -0,0 +1,37 @@
+export function renderHeader() {
+    const header = document.querySelector(".site-header");
+    if (!header) return;
+
+    const token = localStorage.getItem("token");
+    const role = localStorage.getItem("userRole");
+
+    header.innerHTML = `
+    <nav>
+      <a href="../index.html" class="logo">Plot Twist</a>
+      <ul class="nav-links">
+        
+        ${!token ? `
+          <li><a href="/pages/register.html">Login</a></li>
+        ` : `
+          <li><a href="/pages/upload-plant.html">Upload Plant</a></li>
+          <li><a href="/pages/profile.html">Profile</a></li>
+          
+          ${role === "admin" ? `
+            <li><a href="/pages/admin-panel.html" style="color: var(--color-accent)">Admin</a></li>
+          ` : ""}
+          
+          <li><a href="#" id="logoutBtn">Logout</a></li>
+        `}
+      </ul>
+    </nav>
+  `;
+
+    const logoutBtn = document.getElementById("logoutBtn");
+    if (logoutBtn) {
+        logoutBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            localStorage.clear();
+            window.location.href = "../index.html";
+        });
+    }
+}
